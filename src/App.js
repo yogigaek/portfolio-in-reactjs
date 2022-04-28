4cb2231cb2754d1adda429f404a1a2f535a0da4c
Muhammad-yogi <yogikgaek@gmail.com>
2022-04-28 16:51:06 +0700

    form and validation

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import ContentComp from './Components/Styling-components/Content';
 import TableComp from './Components/Styling-components/Table';
 import TableComp2 from './Components/Styling-components/Table2';
 import TableComp3 from './Components/Styling-components/Table3';
-// import FormComp from  './Components/Styling-components/Form.jsx';
 import FooterComp from './Components/Styling-components/Footer';
 import ListComp from './Components/Styling-components/List';
-import Validation from './Components/Form-validation/Validasi';
+import Validasi from './Components/Form-validation/Validation';
 
 function App() {
   return (
@@ -20,7 +19,7 @@ function App() {
       <TableComp />
       <TableComp2 />
       <TableComp3 />
-      <Validation />
+      <Validasi />
       <FooterComp />
     </div>
   );
---
+++ src/Components/Form-validation/Validation.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Validasi() {
+  const initialValues = { username: "", email: "", message: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    // console.log(e.target);
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    // console.log(formValues);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  });
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = `Username is required!`;
+    } else if (values.username.length < 3) {
+      errors.username = `Username must be more than 2 characters`;
+    } else if (values.username.length > 10) {
+      errors.username = `Username cannot esced more than 10 characters`;
+    }
+    if (!values.email) {
+      errors.email = `Email is required!`;
+    } else if (!regex.test(values.email)) {
+      errors.email = `This is not a valid email format !`;
+    }
+    if (!values.message) {
+      errors.message = `Message is required!`;
+    }
+    return errors;
+  };
+
+  return (
+    <div className="container">
+        <form onSubmit={handleSubmit}>
+            <section className="contact" id="kontak1">
+            <h1 style={{ fontSize: "30px" }}>My Contact</h1>
+
+                <div className="kontak1">
+                    <label for="username">Username</label>
+                    <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    value={formValues.username}
+                    onChange={handleChange}
+                    />
+                    <span>{formErrors.username}</span>
+                </div>
+                <div className="kontak1">
+                    <label for="email">Email</label>
+                    <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formValues.email}
+                    onChange={handleChange}
+                    />
+                    <span>{formErrors.email}</span>
+                </div>
+                <div className="kontak1">
+                    <label for="message">Message</label>
+                    <textarea
+                    name="message"
+                    id="pesan"
+                    placeholder="Write Something ..."
+                    value={formValues.message}
+                    style={{ height: "170px" }}
+                    onChange={handleChange}
+                    ></textarea>
+                    <span>{formErrors.message}</span>
+                </div>
+                <div className="kontak1">
+                    <input type="submit" placeholder="Submit" />
+                </div>
+                <br />
+                {Object.keys(formErrors).length === 0 && isSubmit ? (
+                    <div className="message">Data send successfully</div>
+                ) : (
+                    <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+                )}
+            </section>
+        </form>
+    </div>
+  );
+}
+
+export default Validasi;
